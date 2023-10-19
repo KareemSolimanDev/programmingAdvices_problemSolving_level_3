2b04abf30e9619a394cf0dc57ca238cdb5646510
kemooalsayd <kareemalsaydo@gmail.com>
2023-10-19 17:07:10 +0300

    up

---
+++ 2-Sum_matrix_rows.cpp
@@ -0,0 +1,71 @@
+#include <time.h>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int GetRandomNumberInRange(int start, int end)
+{
+    return rand() % (end - start + 1) + start;
+}
+
+void FillMatrixWithRandomNumbers(int matrix[3][3], short rows, short cols)
+{
+    for (short r = 0; r < rows; r++)
+    {
+        for (short c = 0; c < cols; c++)
+        {
+            matrix[r][c] = GetRandomNumberInRange(1, 100);
+        }
+    }
+}
+
+void PrintMatrix(int matrix[3][3], short rows, short cols)
+{
+    for (short r = 0; r < rows; r++)
+    {
+        for (short c = 0; c < cols; c++)
+        {
+            cout << setw(3) << matrix[r][c] << "  ";
+        }
+        cout << endl;
+    }
+}
+int SumRow(int matrix[3][3], short row, short cols)
+{
+    int sum=0;
+
+    for (short c = 0; c < cols; c++)
+    {
+        sum+=matrix[row][c];
+    }
+
+    return sum;
+}
+void PrintSumRows(int matrix[3][3], short rows,short cols)
+{
+    
+
+    for (short r = 0; r < rows; r++)
+    {
+        cout << "Sum of row (" << r+1 << ") : " << SumRow(matrix,r,cols) << endl;
+    }
+
+
+}
+int main()
+{
+    srand(time(0));
+
+    int matrix[3][3];
+
+    FillMatrixWithRandomNumbers(matrix, 3, 3);
+
+    cout << "Thw Matrix ia : " << endl;
+    PrintMatrix(matrix, 3, 3);
+
+    cout << "rows sum : " << endl;
+    PrintSumRows(matrix,3,3);
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # programmingAdvices_problemSolving_level_3
+
+
+#### Notes
+1. prefix A_ means the problem has been solved before but we resolve it by another way
